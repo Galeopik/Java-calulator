390e1e0f013ea0965348ae5fc056ebe1f2a2800d
Galeopik <galeopik@gmail.com>
2024-12-12 22:04:44 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int result;
         System.out.println("Введите данные ");
-        String task = scanner.nextLine();
-        String[] splitTask = task.split(" ");
-        if (splitTask.length > 3) {
-            throw new Exception();
-        }
-        if (splitTask.length < 2) {
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+    }
+    
+    public static String calc(String input) throws Exception {
+        int result;
+        String[] splitTask = input.split(" ");
+        if (splitTask.length < 2 || splitTask.length > 3) {
             throw new Exception("т.к. строка не является математической операцией");
         }
         int num1 = Integer.parseInt(splitTask[0]);
         int num2 = Integer.parseInt(splitTask[2]);
         String operation = splitTask[1];
         if (num1 > 10 || num1 < 1 || num2 > 10 || num2 < 1) {
-            throw new Exception("т.к формат математической операции" +
-                    " не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            throw new Exception("т.к формат математической операции" + " не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
         switch (operation) {
             case "-":
@@ -33,11 +33,8 @@ public class Main {
                 result = num1 / num2;
                 break;
             default:
-                throw new Exception("т.к формат математической операции не удовлетворяет заданию" +
-                        " - два операнда и один оператор (+, -, /, *)");
+                throw new Exception("т.к формат математической операции не удовлетворяет заданию" + " - два операнда и один оператор (+, -, /, *)");
         }
-        System.out.println("Ваш результат " + result);
+        return String.valueOf(result);
     }
 }
-
-
